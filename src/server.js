49beb35d8d606f690f3741a8b267cc52cd360378
Breno Lopes de Souza <breno_lopes_10@yahoo.com.br>
2019-07-31 00:41:47 -0300

    Alteração de argumento com nome errado

--- src/server.js
+++ src/server.js
@@ -15,7 +15,7 @@ const io = require('socket.io')(server);
 
 //quando a aplicação for aberta no front gera uma conexao que recebe o socket 
 io.on('connection', socket => {
-    socket.on('connectionRoom' , box =>{
+    socket.on('connectRoom' , box =>{
         socket.join(box);
     })
 })
